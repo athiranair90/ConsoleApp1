3d1a6fd383fac1d93b7987f1cd54db93959c11d7
Athira Nair <Athira.Nair@atkinsglobal.com>
2021-04-04 10:58:59 +0530

    Add project files.

---
+++ ConsoleApp1/AccoliteProgram.cs
@@ -0,0 +1,180 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class AccoliteProgram
+    {
+        
+
+        //Pattern matching
+
+        public static bool PatternMatching(string str)
+        {
+            var patternSet = str.Split(' ').ToList();
+            bool isCorrect = false;
+            var pattern = patternSet[0];
+            var matchString = patternSet[1];
+
+            for(int i=0,j=0; i< pattern.Length; i++)
+            {
+               switch (pattern[i])
+                {
+                    case '+':
+                        if (Regex.IsMatch(matchString[j].ToString(), @"[A-Za-z]"))
+                        {
+                            j++;
+                        }
+                        else
+                        {
+                            return isCorrect;
+                        }
+                        break;
+                    case '$':
+                        if (Regex.IsMatch(matchString[j].ToString(), @"[0-9]"))
+                        {
+                            j++;
+                        }
+                        else
+                        {
+                            return isCorrect;
+                        }
+                        
+                        break;
+                    case '*':
+                        if (Regex.IsMatch(pattern[i + 1].ToString(), @"[$*+]")){
+                            if(matchString.Substring(j, 3).Distinct().Count() == 1)
+                            {
+                                j = j + 3;
+                            }
+                            else
+                            {
+                                return isCorrect;
+                            }
+                        }
+                        else
+                        {
+                            int.TryParse(pattern[i + 2].ToString(), out int result);
+                            if (matchString.Substring(j, result).Distinct().Count() == 1)
+                            {
+                                j = j + 3;
+                                i = i + 3;
+                            }
+                            else
+                            {
+                                return isCorrect;
+                            }
+                        }
+                        
+                        break;
+                    default:
+                        Console.WriteLine(pattern[i]);
+                        return isCorrect;
+                }
+            }
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// Class object to accept Json data
+        /// </summary>
+        public class DataObject
+        {
+            public string data { get; set; }
+        }
+        /// <summary>
+        /// Get the age above 50 from the API call
+        /// </summary>
+        /// <param name="url">api url</param>
+        public static async Task<int> GetAgeBove50(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("message", nameof(url));
+            }
+            int count = 0;
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(url);
+
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/Json"));
+
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
+
+            //if success
+            if (response.IsSuccessStatusCode)
+            {
+                
+                var data = response.Content.ReadAsStringAsync().Result;
+
+                var dataObjects = JsonConvert.DeserializeObject<DataObject>(data.ToString());
+
+                Console.WriteLine("{0}", dataObjects.data);
+
+                var dataValue =  dataObjects.data.Split(new string[] { "age=","key=",", "," ,"},StringSplitOptions.RemoveEmptyEntries);
+                
+                foreach(var value in dataValue)
+                {
+                    if(int.TryParse(value,out int result)){
+
+                        if (result > 50)
+                        {
+                            count++;
+                        }
+
+                    }
+                }
+                Console.WriteLine("Total number of ages above 50 is : {0}", count);
+
+            }
+            
+            return count;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsMatchingParanthesis(string str)
+        {
+
+            int count = 0;
+            if(str.Length == 0)
+            {
+                return false;
+            }
+
+            for(int i = 0; i < str.Length; i++)
+            {
+
+                if(str[i].Equals('('))
+                {
+                    count++;
+                }
+                else if(str[i].Equals(')'))
+                {
+                    count--;
+                    if(count < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if(count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Formatting;
+
+namespace ConsoleApp1
+{
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Matching Paranthesis
+            string pattern = "))(#(@%^()^#";
+
+            bool isCorrect = AccoliteProgram.IsMatchingParanthesis(pattern);
+
+            #endregion
+
+            #region Pattern matching Test
+            string str = "++*{5} jtggggg";
+            string str1 = "++* jtggg";
+            string str2 = "++* jtggggg";
+            string str3 = "++*{5} jtggggg";
+            Console.WriteLine(AccoliteProgram.PatternMatching(str));
+
+            #endregion
+
+            #region WebAPI Test
+            string URL = "https://coderbyte.com/api/challenges/json/age-counting";
+            AccoliteProgram.GetAgeBove50(URL);
+
+            #endregion
+
+
+            #region  generic Test
+
+
+            int a = 10;
+            double b = 10;
+            float c = 11;
+            long d = 10;
+
+            var x = (a + d) * (c + b);
+            Console.WriteLine(x.GetType());
+            var y = (c + d);
+            Console.WriteLine(y.GetType());
+
+            #endregion
+
+            #region  Check Amstring Number
+
+            Program p = new Program();
+            int number = 153;
+            p.CheckAmstringNumber(number);
+
+            #endregion
+
+            #region  Check factorial
+            int factorialNumber = 5;
+            int factorial = p.FindFactorial(factorialNumber);
+            Console.WriteLine(factorial);
+
+
+            #endregion
+
+            #region  Check Paliandrome
+            //palindrome
+            int checkPal = 121;
+            p.CheckPaliandrome(checkPal);
+
+            #endregion
+
+            #region  Check string Paliandrome
+            //Check string Paliandrome
+            string palString = "abcdef";
+            p.CheckStringPaliandrome(palString);
+            #endregion
+
+            #region  Check factorial
+            //Fibanacci Series
+            number = 15;
+            p.PrintFibonacciSeries(number);
+            #endregion
+
+            #region  Check factorial
+            //Check Prime Number 
+
+            for (int i = 1; i < 10; i++)
+            {
+                p.checkPrimeNumber(i);
+            }
+
+            #endregion
+
+            #region  Check Binary
+            int n = 30;
+            string value = Convert.ToString(n,2);
+
+            if (!value.Contains("1"))
+            {
+                Console.WriteLine("0");
+            }
+
+            var binaryArray =  value.ToCharArray();
+            List<int> binaryCount = new List<int>();
+            for (int i = 0; i < binaryArray.Length; i++)
+            {
+                if (binaryArray[i] == '1')
+                {
+                    i++;
+                    var count = 0;
+                    while (binaryArray[i] != '0')
+                    {
+                        count++;
+                        i++;
+                        
+                    }
+                    binaryCount.Add(count);
+                }
+                
+            }
+
+
+            #endregion
+        }
+
+       
+
+        /// <summary>
+        ///  Check for Prime Number
+        /// </summary>
+        /// <param name="number"></param>
+        public void checkPrimeNumber(int number)
+        {
+            int n = 0;
+            bool flag = false;
+
+            if (number == 1)
+            {
+                Console.WriteLine(number + " is not a prime number");
+            }
+
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(number+" is not a prime number");
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+            {
+                Console.WriteLine(number + " is a prime number");
+            }
+
+        }
+
+
+        /// <summary>
+        /// Reversed a string / Paliandrome
+        /// </summary>
+        /// <param name="str"></param>
+        public void CheckStringPaliandrome(string str)
+        {
+
+            string reverse = String.Empty;
+            Console.WriteLine("Correct String " + str);
+            for (int i = str.Length-1; i >= 0; i--)
+            {
+                reverse = reverse + str[i];
+            }
+            Console.WriteLine("Reversed String "+ reverse);
+
+            if(str == reverse)
+            {
+                Console.WriteLine("String is a palindrome.\n");
+            }
+            else
+            {
+                Console.WriteLine("String is not a palindrome.\n");
+            }
+        }
+
+
+
+        /// <summary>
+        /// Check is number Paliandrome
+        /// </summary>
+        /// <param name="number"></param>
+        public void CheckPaliandrome(int number)
+        {
+
+            int reverse =0;
+            int temp = number;
+            while (temp != 0)
+            {
+                reverse = reverse * 10;
+                reverse = reverse + temp % 10;
+                temp = temp / 10;
+            }
+
+            if (number == reverse)
+            {
+                Console.WriteLine("number is a palindrome number.\n");
+            }
+            else
+            {
+                Console.WriteLine("number is not a palindrome number.\n");
+            }
+        }
+
+
+        /// <summary>
+        /// Print Fibonacci Series
+        /// </summary>
+        /// <param name="number"></param>
+        public void PrintFibonacciSeries(int number)
+        {
+            int n1=0, n2=1, n3;
+
+            StringBuilder str = new StringBuilder();
+            str.Append(n1+" "+n2 +" ");
+
+            for (int i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                str.Append(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.WriteLine(str.ToString());
+        }
+
+
+        /// <summary>
+        ///   Factorial
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public int FindFactorial(int number)
+        {
+            if (number==0)
+            {
+                return 1;
+            }
+
+            if (number == 1)
+            {
+                return 1;
+            }
+
+            return number * FindFactorial(number - 1);
+        }
+
+
+        /// <summary>
+        /// Amstrong Number
+        /// </summary>
+        /// <param name="number"></param>
+        public void CheckAmstringNumber(int number)
+        {
+            int rem;
+            int sum = 0;
+            for (int i = number; i > 0; i = i / 10)
+            {
+                rem = i % 10;
+                sum += rem * rem * rem;
+            }
+
+            if (number == sum)
+            {
+                Console.WriteLine("The number is Amstrong Number");
+            }
+            else
+            {
+                Console.WriteLine("The number not an Amstrong Number");
+            }
+        }
+    }
+}
